71500d4d3a4b1b70b525c24328e5f0b92b580b94
nmelhuish759 <65666056+nmelhuish759@users.noreply.github.com>
2025-04-30 16:33:46 -0400

    Update xlsx.js

--- xlsx.js
+++ xlsx.js
@@ -1,4 +1,4 @@
-﻿/*! xlsx.js (C) 2013-present SheetJS -- http://sheetjs.com */
+/*! xlsx.js (C) 2013-present SheetJS -- http://sheetjs.com */
 /* vim: set ts=2: */
 /*exported XLSX */
 /*global global, exports, module, require:false, process:false, Buffer:false, ArrayBuffer:false */
